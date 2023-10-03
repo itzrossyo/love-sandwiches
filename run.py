54221751282a19bff877fb7b028cc6efb5475b54
itzrossyo <Rossespley@gmail.com>
2023-10-03 06:13:43 +0100

    added uerinput for getting data

--- run.py
+++ run.py
@@ -5,15 +5,23 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    get sales figures input from the user
+    """
 
-print(data)
+    print('Please enter sales data from the last market.')
+    print('Data should be six number, separated by commas.')
+    print('Example: 10,20,30,40,50.50\n')
+    data_str = input('Enter you data here:')
+    print(f"the data provided is {data_str}")
+
+get_sales_data()
